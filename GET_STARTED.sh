9c1498ac29bed83be3a5014995e44b3d6b9df832
Matt W <89744263+mgwein@users.noreply.github.com>
2022-11-01 14:00:29 -0400

    Update GET_STARTED.sh

--- GET_STARTED.sh
+++ GET_STARTED.sh
@@ -1,3 +1,6 @@
 cmake . -B build
 cmake --build build --config RelWithDebInfo -j
-.\build\testbed --scene data\ball.\build\testbed --scene data\ball
+data-folder$ python [path-to-instant-ngp]/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 16
+data-folder$ python [path-to-instant-ngp]/scripts/colmap2nerf.py --video_in <filename of video> --video_fps 2 --run_colmap --aabb_scale 16
+.\build\testbed --scene data\ball
+instant-ngp$ ./build/testbed --mode nerf --scene [path to training data folder containing transforms.json]
