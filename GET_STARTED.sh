5c861443fc7e040072b16b14026f977b24fb9b79
Matt W <89744263+mgwein@users.noreply.github.com>
2022-11-30 11:44:44 -0500

    asdf

--- GET_STARTED.sh
+++ GET_STARTED.sh
@@ -1,5 +1,6 @@
 cmake . -B build
 # cmake . -D TCNN_CUDA_ARCHITECTURES=86 -B build
+# CUDA_VISIBLE_DEVICES=0
 cmake --build build --config RelWithDebInfo -j
 data-folder$ python [path-to-instant-ngp]/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 16
 data-folder$ python [path-to-instant-ngp]/scripts/colmap2nerf.py --video_in <filename of video> --video_fps 2 --run_colmap --aabb_scale 16
